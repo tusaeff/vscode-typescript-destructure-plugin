091dfc625ff7383218741f83eb62ea12c5140032
tusaeff <tusaev.dv@phystech.edu>
2020-07-01 10:08:40 +0300

    :hammer: Fixed prepublish scripts

--- vscode-extension/scripts/sync-plugin-version.js
+++ vscode-extension/scripts/sync-plugin-version.js
@@ -8,33 +8,54 @@ const merge = require('lodash/merge');
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
-const readPackageJSON = async (dirPath) => {
-  const json = await readFile(
-    path.join(process.cwd(), dirPath, 'package.json'),
-    { encoding: 'utf-8' },
-  );
+const readJSON = async (filepath) => {
+  const json = await readFile(path.join(process.cwd(), filepath), {
+    encoding: 'utf-8',
+  });
 
   return JSON.parse(json);
-}
+};
 
-const writePackageJSON = async (dirPath, content) => {
+const writeJSON = async (filepath, content) => {
   return writeFile(
-    path.join(process.cwd(), dirPath, 'package.json'),
+    path.join(process.cwd(), filepath),
     JSON.stringify(content, null, '\t'),
-    { encoding: 'utf-8' },
+    { encoding: 'utf-8' }
   );
-}
+};
 
-const updatePackageJSON = async (dirPath, update) => {
-  const oldPackageJSON = await readPackageJSON(dirPath);
-  
-  return writePackageJSON(dirPath, merge(oldPackageJSON, update));
-}
+const updateJSON = async (path, update) => {
+  const oldJSON = await readJSON(path);
+
+  const newContent =
+    typeof update == 'function'
+      ? update(oldJSON)
+      : merge(oldJSON, update);
+
+  return writeJSON(path, newContent);
+};
+
+const makeNPMPackageUrl = (name, version) => `https://registry.npmjs.org/${name}/-/${name}-${version}.tgz`
 
 const addPluginToExtensionDependencies = async () => {
-  const plugin = await readPackageJSON('../typescript-plugin');
+  const plugin = await readJSON('../typescript-plugin/package.json');
 
-  return updatePackageJSON('./', { dependencies: { [plugin.name]: plugin.version } });
-}
+  await updateJSON('./package.json', {
+    dependencies: { [plugin.name]: plugin.version },
+  });
+
+  await updateJSON(
+    './package-lock.json',
+    (oldJSON) => {
+      oldJSON.dependencies[plugin.name] = {
+        version: plugin.version,
+        resolved: makeNPMPackageUrl(plugin.name, plugin.version),
+      };
+
+      return oldJSON;
+    },
+    true
+  );
+};
 
 module.exports = addPluginToExtensionDependencies;
